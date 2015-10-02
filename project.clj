92eaf57d7703fa685ba58f635bf219d3dbce71a2
zjhmale <zjhmale@gmail.com>
2015-10-03 01:33:54 +0800

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject colortest "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-colortest "0.1.0"
+  :description "a leiningen plugin to work with acolfut"
+  :url "https://github.com/zjhmale/lein-colortest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[jansi-clj "0.1.0"]]
